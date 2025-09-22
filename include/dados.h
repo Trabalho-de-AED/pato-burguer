74eb64b1aeff8d2790cb9275fc2554b93ed30f69
Atila Bezerra Almeida <atila.almeida@simberg.com.br>
2025-09-21 23:00:39 -0300

    chore: remoção de comentários

--- include/dados.h
+++ include/dados.h
@@ -7,11 +7,9 @@
 #define MAX_INGREDIENTES 10
 #define MAX_HAMBURGUERS 12
 
-// Arrays globais de ingredientes e cardápio
 extern Ingrediente ingredientes[MAX_INGREDIENTES];
 extern Hamburguer cardapio[MAX_HAMBURGUERS];
 
-// Função para inicializar os dados
 void inicializa_dados();
 
 #endif
